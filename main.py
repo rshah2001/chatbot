e5cfec15d1d2582e26e18253ce6386efd7317799
pavankumarm2505 <pavankumarm2505@gmail.com>
2024-08-14 13:00:03 -0400

    Full code for simple Chatbot

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ class ChatBotWindow(QMainWindow):
         self.ai_chat = Chatbot()
         self.setMinimumSize(700, 500)
 
+
         # Add chat area widget
         self.chat_area = QTextEdit(self)
         self.chat_area.setGeometry(10, 10, 680, 320)
